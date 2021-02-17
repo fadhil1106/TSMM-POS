fa81bc40a707d4b72ba253109bbcee4d88bb2ff1
fadhil1106 <fuadf.512@gmail.com>
2021-02-17 12:55:25 +0800

    update view update DB

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://tsmm-pos.test/';
+$config['base_url'] = 'http://172.16.14.8/tsmm-pos/';
 
 /*
 |--------------------------------------------------------------------------
